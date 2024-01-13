1fba2d1a0029a7c0fb1a2a1e3a1cde3c36cec4e9
lym <1002078582@qq.com>
2024-01-13 21:32:45 +0800

    feat: add static fucntion

--- MyPromise.js
+++ MyPromise.js
@@ -117,6 +117,28 @@ class MyPromise {
   }
 
   /**
+   * 仅处理失败的场景
+   * @param {Function} onRejected 
+   */
+  catch(onRejected) {
+    return this.then(null, onRejected);
+  }
+
+  /**
+   * 无论成功还是失败都会执行回调
+   * @param {Function} onSettled 
+   */
+  finally(onSettled) {
+    return this.then((data) => {
+      onSettled();
+      return data;
+    }, (reason) => {
+      onSettled();
+      throw reason;
+    });
+  }
+
+  /**
    * 更改任务状态
    * @param {String} newState 新状态
    * @param {any} value 相关数据
@@ -148,10 +170,110 @@ class MyPromise {
     // 改变状态和数据
     this._changeState(REJECTED, reason);
   }
+
+  /**
+   * 返回一个已完成的Promise
+   * @param {any} data 
+   * 特殊情况：
+   * 1. 传递的data本身就是ES6的Promise对象
+   * 2. 传递的data是PromiseLike（符合PromiseA+规范）
+   */
+  static resolve(data) {
+    if(data instanceof MyPromise) {
+      return data;
+    }
+    return new MyPromise((resolve, reject) => {
+      if(isPromise(data)) {
+        data.then(resolve, reject);
+      }else {
+        resolve(data);
+      }
+    })
+  }
+
+  /**
+   * 返回一个被拒绝的Promise
+   * @param {any} reason 
+   */
+  static reject(reason) {
+    return new MyPromise((resolve, reject) => {
+      reject(reason);
+    })
+  }
+
+  /**
+   * 得到一个新的Promise
+   * 该Promise的状态取决于proms的执行
+   * @param {iterator} proms 是一个迭代器，包含多个Promise
+   */
+  static all(proms) {
+    return new MyPromise((resolve, reject) => {
+      try {
+        const results = [];
+        let count = 0; // Promise总数
+        let fulfilledCount = 0; // Promise已完成的数量
+        for(const promise of proms) {
+          let i = count;
+          count++;
+          MyPromise.resolve(promise).then(data => {
+            fulfilledCount++;
+            results[i] = data; 
+            if(fulfilledCount === count) {
+              // 当前是最后一个promise完成了
+              resolve(results);
+            }
+          }, reject)
+        }
+        if(count === 0) {
+          resolve(results);
+        }
+      } catch(error) {
+        reject(error);
+      }
+    })
+  }
+
+  /**
+   * 等待所有Promise有结果后，返回Promise完成
+   * @param {iterator} proms 
+   */
+  static allSettled(proms) {
+    const ps = [];
+    for(const p of proms) {
+      ps.push(
+        MyPromise.resolve(p).then(
+          (value) => ({
+            status: FULFILLED,
+            value
+          }),
+          (reason) => ({
+            status: FULFILLED,
+            reason
+          })
+        )
+      )
+    }
+    return MyPromise.all(ps);
+  }
+
+  /**
+   * 返回的Promise与第一个有结果的一致
+   * @param {iterator} proms 
+   */
+  static race(proms) {
+    return MyPromise((resolve, reject) => {
+      for(const p of proms) {
+        MyPromise.resolve(p).then(resolve, reject);
+      }
+    })
+  }
 }
 
+// MyPromise.prototype.catch = function() {}
+// MyPromise.resolve = function() {}
+
 const promise = new MyPromise((resolve, reject) => {
-  // resolve(123)
-  throw new Error(123)
+  resolve(123)
+  // throw new Error(123)
 })
 console.log(promise)
